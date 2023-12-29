c8e4a4fe6b7461e6141bc03b7c52adecea2ca5da
eona1301 <eona1301@gmail.com>
2023-12-30 00:38:01 +0900

    🎉 init: begin a wish erumi backend project

---
+++ src/test/java/site/erumi/api/WisherumiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.erumi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisherumiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
